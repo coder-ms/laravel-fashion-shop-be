b851e339a6b056a1b20802f58030ea8e87816a9f
adrianobarboza31 <barbozaa022@gmail.com>
2023-01-25 15:24:59 +0100

    add foreign keys

---
+++ database/migrations/2023_01_25_140044_add_texture_id_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('texture_id')->nullable();
+            $table->foreign('texture_id')->references('id')->on('textures');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_texture_id_foreign');
+            $table->dropColumn('texture_id');
+        });
+    }
+};
