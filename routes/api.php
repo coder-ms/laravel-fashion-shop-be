250377aa4be19538b77f2a963c3459fda1e2d652
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-28 14:52:43 +0100

    add lead

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\LeadController;
 
 
 /*
@@ -23,4 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 
-
+Route::post('/contacts', [LeadController::class, 'store']);
